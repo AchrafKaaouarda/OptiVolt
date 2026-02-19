9fe2a1f5fe53715d4b7fe794c062363b7bbafe1e
med1221 <Mohamedbouchaara5@gmail.com>
2026-02-19 13:39:40 +0000

    creation table user -mohammed

--- Schema.sql
+++ Schema.sql
@@ -1,2 +1,19 @@
 CREATE DATABASE IF NOT EXISTS optivolt DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE optivolt;
+-- TABLES --
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role ENUM(
+        'CLIENT',
+        'ENTREPRISE',
+        'ADMIN'
+    ) NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    telephone VARCHAR(20),
+    password_hash VARCHAR(255) NOT NULL,
+    ville VARCHAR(50),
+    adresse TEXT,
+    is_banned BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
